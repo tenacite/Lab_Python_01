2fd0cc71e245e14f76f9d3e54e2043738698fad6
Nii Logoligi <niilogoligi@niilogoligi-Satellite-C655.(none)>
2012-06-20 17:05:03 +0000

    will send the finished one soon

---
+++ userinput.py
@@ -0,0 +1,8 @@
+first = raw_input("Enter your first name: ")
+last = raw_input("Enter your last name: ")
+print "Enter your date of birth: "
+m = raw_input("Month? ")
+d = raw_input("Day? ")
+y = raw_input("Year? ")
+print first+" "+last+" was born on "+m+" "+str(d)+", "+str(y)	
+
